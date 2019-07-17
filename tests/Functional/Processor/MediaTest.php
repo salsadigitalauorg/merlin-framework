70465681c493411725a6b9648394a8139f01a866
Steve Worley <sj.worley88@gmail.com>
2019-07-17 10:14:58 +1000

    Ensures that media process correctly moves configuration values.
    - The trait expects configuration to be under `attributes` key on the config array. The processors base constructor does variable assignment which was overriding the custom pproperty allocation the processor does to conform with the trait.
    
    Fixes #23

---
+++ tests/Functional/Processor/MediaTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Migrate\Tests\Functional\Processor;
+
+use Migrate\Tests\Functional\CrawlerTestCase;
+use Migrate\Processor\Media;
+
+/**
+ * Ensure that the media trait correctly functions.
+ */
+class MediaTest extends CrawlerTestCase
+{
+
+  /**
+   * Ensure that when used as a proceessor getEmbeddedAttributes returns valid.
+   */
+  public function testMediaAttributesProcessor()
+  {
+    $config = [
+      'type' => 'media',
+      'selector' => 'img',
+      'file' => 'src',
+      'name' => 'src',
+      'alt' => 'alt',
+      'data_embed_button' => 'test_media',
+      'data_entity_embed_display' => 'view_mode:media.test',
+      'data_entity_type' => 'test',
+    ];
+
+    $crawler = $this->getCrawler();
+
+    $processor = new Media($config, $crawler, $this->getOutput());
+
+    foreach ($processor->getEmbeddedAttributes() as $ak => $av) {
+      $this->assertEquals($av, $config[$ak], "$ak");
+    }
+  }
+
+  /**
+   * Ensure that the processor value matches whats expected.
+   */
+  public function testProcessorValue()
+  {
+    $config = [
+      'type' => 'media',
+      'selector' => 'img',
+      'file' => 'src',
+      'name' => 'src',
+      'alt' => 'alt',
+      'data_embed_button' => 'test_media',
+      'data_entity_embed_display' => 'view_mode:media.test',
+      'data_entity_type' => 'test',
+    ];
+
+    $crawler = $this->getCrawler();
+
+    $processor = new Media($config, $crawler, $this->getOutput());
+
+    $markup = $crawler->filter('#with-image')->html();
+    $value = $processor->process($markup);
+
+    $this->assertTrue(strpos($value, 'data-embed-button="test_media"') !== FALSE);
+    $this->assertTrue(strpos($value, 'data-entity-type="test"') !== FALSE);
+    $this->assertTrue(strpos($value, 'data-entity-embed-display="view_mode:media.test"') !== FALSE);
+    $this->assertTrue(strpos($value, '<drupal-entity') !== FALSE);
+  }
+
+
+}
