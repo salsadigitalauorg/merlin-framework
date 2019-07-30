46593fe0c8245c62b71ad04ea59b211c887a1210
Stuart Rowlands <stuart@firecannon.com>
2019-07-30 13:14:40 +1000

    Fixed phpcs.

--- src/Processor/ReplaceSmartQuotes.php
+++ src/Processor/ReplaceSmartQuotes.php
@@ -17,28 +17,28 @@ class ReplaceSmartQuotes implements ProcessorInterface
      */
     public function process($value)
     {
-        $chr_map = array(
-          "\xC2\x82" => "'",
-          "\xC2\x84" => '"',
-          "\xC2\x8B" => "'",
-          "\xC2\x91" => "'",
-          "\xC2\x92" => "'",
-          "\xC2\x93" => '"',
-          "\xC2\x94" => '"',
-          "\xC2\x9B" => "'",
-          "\xC2\xAB" => '"',
-          "\xC2\xBB" => '"',
-          "\xE2\x80\x98" => "'",
-          "\xE2\x80\x99" => "'",
-          "\xE2\x80\x9A" => "'",
-          "\xE2\x80\x9B" => "'",
-          "\xE2\x80\x9C" => '"',
-          "\xE2\x80\x9D" => '"',
-          "\xE2\x80\x9E" => '"',
-          "\xE2\x80\x9F" => '"',
-          "\xE2\x80\xB9" => "'",
-          "\xE2\x80\xBA" => "'",
-        );
+        $chr_map = [
+            "\xC2\x82"     => "'",
+            "\xC2\x84"     => '"',
+            "\xC2\x8B"     => "'",
+            "\xC2\x91"     => "'",
+            "\xC2\x92"     => "'",
+            "\xC2\x93"     => '"',
+            "\xC2\x94"     => '"',
+            "\xC2\x9B"     => "'",
+            "\xC2\xAB"     => '"',
+            "\xC2\xBB"     => '"',
+            "\xE2\x80\x98" => "'",
+            "\xE2\x80\x99" => "'",
+            "\xE2\x80\x9A" => "'",
+            "\xE2\x80\x9B" => "'",
+            "\xE2\x80\x9C" => '"',
+            "\xE2\x80\x9D" => '"',
+            "\xE2\x80\x9E" => '"',
+            "\xE2\x80\x9F" => '"',
+            "\xE2\x80\xB9" => "'",
+            "\xE2\x80\xBA" => "'",
+        ];
 
         $chr = array_keys($chr_map);
         $rpl = array_values($chr_map);
