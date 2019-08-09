9e53a80b20f099860f18c824704fe40f7a7db2ad
Andrew Rowlands <andrew@firecannon.com>
2019-08-09 20:09:39 +1000

    Settings for inclusion and exclusion of query and fragment for #47

--- src/Type/Alias.php
+++ src/Type/Alias.php
@@ -18,18 +18,51 @@ class Alias extends TypeBase implements TypeInterface
      */
     public function process()
     {
-        $parts = parse_url($this->crawler->getUri());
+        $uri = $this->crawler->getUri();
+        $parts = parse_url($uri);
 
-        // Throw away domain, scheme etc.
+        $mainConfig = $this->output->getConfig();
+        $includeQuery = ($mainConfig->get('url_options')['include_query'] ?? false);
+        $includeFrag  = ($mainConfig->get('url_options')['include_fragment'] ?? false);
+
+        // Check for specific url overrides for the query and fragment options.
+        $overrideUrls = ($mainConfig->get('url_options')['urls'] ?? null);
+        if (is_array($overrideUrls)) {
+          foreach ($overrideUrls as $idx => $overrideUrl) {
+            if ($overrideUrl['url'] == self::getDomainlessUrl($uri)) {
+              $includeQuery = ($overrideUrl['include_query'] ?? false);
+              $includeFrag  = ($overrideUrl['include_fragment'] ?? false);
+            }
+          }
+        }
+
+        // Throw away domain, scheme etc and rebuild according to config options.
         $path  = isset($parts['path']) ? $parts['path'] : null;
-        $query = isset($parts['query']) ? "?".$parts['query'] : null;
-        $frag  = isset($parts['fragment']) ? "#".$parts['fragment'] : null;
+        $query = isset($parts['query']) && ($includeQuery || $includeFrag) ? "?".$parts['query'] : null;
+        $frag  = isset($parts['fragment']) && $includeFrag ? "#".$parts['fragment'] : null;
 
         $url = "{$path}{$query}{$frag}";
 
         $this->addValueToRow($url);
 
     }//end process()
+
+
+    /**
+     * Returns the path query and fragment component of the url.
+     * @param $uri
+     *
+     * @return string
+     */
+    public static function getDomainlessUrl($uri) {
+      $parts = parse_url($uri);
+      $path  = isset($parts['path']) ? $parts['path'] : null;
+      $query  = isset($parts['query']) ? "?".$parts['query'] : null;
+      $frag  = isset($parts['fragment']) ? "#".$parts['fragment'] : null;
+      $u = "{$path}{$query}{$frag}";
+      return $u;
+
+    }//end getDomainlessUrl()
 
 
 }//end class
