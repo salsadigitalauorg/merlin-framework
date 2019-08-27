407603895c94a867b9711f74866e19f8d70c5a35
Nick Georgiou <nickgeorgi@gmail.com>
2019-08-27 11:24:46 +1000

    Remove line

--- src/Output/OutputBase.php
+++ src/Output/OutputBase.php
@@ -148,7 +148,6 @@ abstract class OutputBase implements OutputInterface
         $ext    = $this->ext;
         $append = !empty($this->config->get('runner')['append']);
 
-        print_r($this->data);
         foreach ($this->data as $file => $data) {
             $filename = $dir ? "$dir/$file.$ext" : "$file.$ext";
 
