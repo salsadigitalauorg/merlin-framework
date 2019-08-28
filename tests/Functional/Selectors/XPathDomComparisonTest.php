e4e176e27d97c4b1799283744ba7071d0633bc8f
Nick Georgiou <nickgeorgi@gmail.com>
2019-08-28 11:29:25 +1000

    Write some tests for Types

---
+++ tests/Functional/Selectors/XPathDomComparisonTest.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Migrate\Tests\Functional\Type;
+
+use Migrate\Tests\Functional\CrawlerTestCase;
+use Migrate\Type\Text;
+use Migrate\Type\LongText;
+use Migrate\Type\Link;
+
+/**
+ * Ensure that the XPath and DOM selectors yield the same results for the same selectors
+ */
+class XPathDomComparisonTest extends CrawlerTestCase
+{
+
+  /**
+   * Ensure consistent output for XPath and DOM selectors for Link type
+   */
+  public function testLink()
+  {
+    // DOM
+    $config_dom = [
+      'field' => 'field',
+      'selector' => 'ul.with-links > li'
+    ];
+    $row_dom = new \stdClass();
+    $type_dom = new Link(
+      $this->getCrawler(),
+      $this->getOutput(),
+      $row_dom,
+      $config_dom
+    );
+
+    $type_dom->process();
+
+    // XPath (selector converted using https://css-selector-to-xpath.appspot.com/)
+    $config_xpath = [
+      'field' => 'field',
+      'selector' => '//ul[contains(concat(" ",normalize-space(@class)," ")," with-links ")]/li'
+    ];
+    $row_xpath = new \stdClass();
+    $type_xpath = new Link(
+      $this->getCrawler(),
+      $this->getOutput(),
+      $row_xpath,
+      $config_xpath
+    );
+
+    $type_xpath->process();
+    
+    // Compare DOM and XPath
+    $this->assertEquals($row_dom->field, $row_xpath->field);
+  }
+
+  /**
+   * Ensure consistent output for XPath and DOM selectors for LongText type
+   */
+  public function testLongText()
+  {
+    // DOM
+    $row_dom = new \stdClass();
+    $type_dom = new LongText(
+      $this->getCrawler(),
+      $this->getOutput(),
+      $row_dom,
+      ['field' => 'field_body', 'selector' => '.main-content']
+    );
+
+    $type_dom->process();
+
+    // XPath (selector converted using https://css-selector-to-xpath.appspot.com/)
+    $row_xpath = new \stdClass();
+    $type_xpath = new LongText(
+      $this->getCrawler(),
+      $this->getOutput(),
+      $row_xpath,
+      ['field' => 'field_body', 'selector' => '//*[contains(concat(" ",normalize-space(@class)," ")," main-content ")]']
+    );
+
+    $type_xpath->process();
+
+    // Compare DOM and XPath
+    $this->assertEquals($row_dom->field_body, $row_xpath->field_body);
+  }
+
+  /**
+   * Ensure consistent output for XPath and DOM selectors for Text type
+   */
+  public function testText()
+  {
+    // DOM
+    $row_dom = new \stdClass();
+    $type_dom = new Text(
+      $this->getCrawler(),
+      $this->getOutput(),
+      $row_dom,
+      ['field' => 'title', 'selector' => 'h1']
+    );
+
+    $type_dom->process();
+
+    // XPath
+    $row_xpath = new \stdClass();
+    $type_xpath = new Text(
+      $this->getCrawler(),
+      $this->getOutput(),
+      $row_xpath,
+      ['field' => 'title', 'selector' => '//h1']
+    );
+
+    $type_xpath->process();
+
+    // Compare DOM and XPath
+    $this->assertEquals($row_dom->title, $row_xpath->title);
+  }
+
+}
