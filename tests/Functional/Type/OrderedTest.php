d0c1c7111436e02ad021057b0e06aab6122bcff7
Stuart Rowlands <srowlands@users.noreply.github.com>
2019-08-31 08:38:32 +1000

    Use same output structure for dom and xpath (long_text) (#75)

---
+++ tests/Functional/Type/OrderedTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Migrate\Tests\Functional\Type;
+
+use Migrate\Tests\Functional\CrawlerTestCase;
+use Migrate\Type\Ordered;
+use Migrate\Exception\ElementNotFoundException;
+
+class OrderedTest extends CrawlerTestCase {
+
+  private $config = [
+    'field' => 'ordered',
+    'type' => 'ordered',
+    'selector' => 'ul.with-links > li',
+    'available_items' => [
+      [
+        'by' => [
+          'attr' => 'class',
+          'text' => 'content'
+        ],
+        'fields' => [
+          [
+            'field' => 'field_body',
+            'type' => 'longtext',
+          ]
+        ],
+      ]
+    ],
+  ];
+
+  /**
+   * Test valid configuration.
+   */
+  public function testValidConfiguration() {
+    $row = new \stdClass;
+    $type = new Ordered(
+      $this->getCrawler(),
+      $this->getOutput(),
+      $row,
+      $this->config
+    );
+    $type->process();
+    $this->assertObjectHasAttribute('ordered', $row);
+    $this->assertEquals(9, count($row->ordered['children']));
+  }
+
+  /**
+   * Test available_items missing.
+   */
+  public function testAvailableItemsMissing() {
+    $row = new \stdClass;
+    $config = [
+      'field' => 'ordered',
+      'type' => 'ordered',
+      'selector' => 'ul.with-links > li'
+    ];
+    $type = new Ordered(
+      $this->getCrawler(),
+      $this->getOutput(),
+      $row,
+      $config
+    );
+    $this->expectException(\Exception::class);
+    $type->process();
+  }
+
+}
