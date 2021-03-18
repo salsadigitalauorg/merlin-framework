628ede669a4a331b53f6d8b59c71ce13c0bfbc26
Andrew Rowlands <andrew@firecannon.com>
2021-03-18 02:13:05 +0000

    Array config holder for sub_fetch processor

---
+++ src/Parser/ArrayConfig.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Merlin\Parser;
+
+
+class ArrayConfig extends ConfigBase
+{
+
+
+  /**
+   * Build a configuration object.
+   */
+  public function __construct($data)
+  {
+    $this->data = $data;
+  }//end __construct()
+
+
+  public function parse() {
+    // Nada
+    return $this;
+  }
+
+}
