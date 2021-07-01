c0d6f37d84574c7ab2708dd6a596e03b7a507ad0
Andrew Rowlands <andrew@firecannon.com>
2021-07-01 23:13:49 +0100

    Remove old unused functions.

--- src/Type/Group.php
+++ src/Type/Group.php
@@ -7,17 +7,13 @@ use Merlin\Command\GenerateCommand;
 use Symfony\Component\DomCrawler\Crawler;
 use Merlin\Utility\MerlinUuid;
 
-use function DeepCopy\deep_copy;
-
-
 /**
  * The Group class lets you build a flattened, but grouped result set from
  * a nested DOM structure.  You can make your result nested, too, if you
  * wish by nesting groups.
  *
- * NOTE: Items in the group are NOT linked to their ancestors!
- * i.e. you cannot use parent, ../ or ancestor xpath commands.  This is
- * because it creates a new DOMDocument with the group to operate on.
+ * NOTE: Items in the group are NOT linked to their ancestors.
+ * i.e. you cannot use parent, ../ or ancestor xpath commands.
  *
  * Class Group
  */
@@ -65,79 +61,29 @@ class Group extends TypeBase implements TypeInterface {
 
   /**
    * {@inheritdoc}
+   * @throws \Exception
    */
   public function process()
   {
 
-    $items = isset($this->config['each']) ? $this->config['each'] : [];
-    $options = isset($this->config['options']) ? $this->config['options'] : [];
+    $items = ($this->config['each'] ?? []);
+    $options = ($this->config['options'] ?? []);
 
     if (empty($items)) {
       throw new \Exception('"each" key required for group.');
     }
 
     $results = [];
-
-    // HERE BE MADNESS! This works but is hopefully not required.
-    // Here we create a new document and then processes a
-    // node as though it was separate by removing its siblings and then
-    // rinse, wash, repeat for each sibling.
-    /*
-    foreach ($this->crawler as $content) {
-      $selector = $this->config['selector'];
-      $original_nodes = $this->crawler->evaluate($selector);
-
-      foreach ($original_nodes as $idx => $node) {
-        // We are going to modify the DOM so use a new document per node we want to process.
-        $c = new Crawler($this->crawler->html(), $this->crawler->getUri(), $this->crawler->getBaseHref());
-
-        // The original crawler object has private access on the document.  Of course.
-        $patch = function() use ($items) {
-          return $this->document;
-        };
-        $doc = $patch->call($c);
-
-        // Find our group selector and remove each one unless
-        // it's the current one in the outer loop.  (i.e. we
-        // go over it and remove siblings for each sibling).
-        // This lets us process them individually.
-        $xpath = new \DOMXPath($doc);
-        $nodes = $xpath->evaluate($selector);
-
-        for ($i = $nodes->length; --$i >= 0;) {
-          if ($i != $idx) {
-            $n = $nodes->item($i);
-            $n->parentNode->removeChild($n);
-          }
-        }
-
-        // Iterate over our config child items and process.
-        foreach ($items as $item) {
-          // Todo: Allow global scope for items option i.e. make this concat bit optional.
-          $item['selector'] = $selector.$item['selector'];
-
-          $row = new \stdClass();
-          $this->processItem($row, $c, $item);
-          $tmp[$item['field']] = @$row->{$item['field']};
-        }
-
-        $results[] = $tmp;
-      }//end foreach
-    }//end foreach
-    */
-
-    // More sensible approach using subcrawler.
-    foreach ($this->crawler as $content) {
+    foreach ($this->crawler as $_content) {
       $selector = $this->config['selector'];
 
       $group_nodes = $this->crawler->evaluate($selector);
 
       foreach ($group_nodes->getIterator() as $n) {
-
         $cc = new Crawler($n, $this->crawler->getUri(), $this->crawler->getBaseHref());
         $tmp = [];
 
-        foreach ($items as $idx => $item) {
+        foreach ($items as $_idx => $item) {
           $row = new \stdClass();
           $this->processItem($row, $cc, $item);
 
@@ -148,7 +94,6 @@ class Group extends TypeBase implements TypeInterface {
           }
 
           $tmp[$item['field']] = @$row->{$item['field']};
-
         }
 
         $serialised = json_encode($tmp);
@@ -159,36 +104,9 @@ class Group extends TypeBase implements TypeInterface {
         $uuid = MerlinUuid::getUuid($uuid_key);
         $tmp['uuid'] = $uuid;
 
-
         $results[] = $tmp;
-      }
-    }
-
-    // Original quick approach.  Types that need to process parent nodes
-    // though won't work!  (e.g. Media replace).
-    /*
-        $this->crawler->each(
-        function(Crawler $node) use ($items, &$results) {
-
-          $nodes = $node->filterXPath($this->config['selector']);
-
-          foreach ($nodes as $n) {
-            $tmp = [];
-            foreach ($items as $item) {
-              $doc = new \DOMDocument();
-              $in = $doc->importNode($n, true);
-              $doc->appendChild($in);
-              $c = new Crawler($doc, $this->crawler->getUri(), $this->crawler->getBaseHref());
-              $row = new \stdClass();
-              $this->processItem($row, $c, $item);
-              $tmp[$item['field']] = @$row->{$item['field']};
-            }
-
-            $results[] = $tmp;
-          }//end foreach
-        }
-        );
-    */
+      }//end foreach
+    }//end foreach
 
     $sortField = ($options['sort_field'] ?? null);
     if (!empty($sortField)) {
@@ -198,16 +116,14 @@ class Group extends TypeBase implements TypeInterface {
 
     if (empty($options['exclude_from_output'])) {
       $this->row->{$this->config['field']} = [
-        'type' => 'group',
-        'children' => $results,
+          'type'     => 'group',
+          'children' => $results,
       ];
     }
 
-    // NOTE: This does not currently support NESTED paragraph generation, would need to do like
-    // NOTE: Iterations over 'children' or something (or any children found with type group).
-
+    // NOTE: This does not currently support NESTED output generation.
     if (key_exists('output_filename', $options)) {
-      // The output filename could be e.g. 'standard_page_paragraphs'
+      // The output filename could be e.g. 'standard_page_paragraphs'.
       $this->output->mergeRow($options['output_filename'], 'data', $results, true);
 
       $group_uuids = [];
@@ -222,8 +138,7 @@ class Group extends TypeBase implements TypeInterface {
       }
     }
 
-
-  }//end process()
+}//end process()
 
 
   /**
