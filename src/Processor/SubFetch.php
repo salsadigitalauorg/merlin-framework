cfbe30e8547aff33c1dac844c72fe96ca66bb373
Andrew Rowlands <andrew@firecannon.com>
2021-07-03 14:57:44 +0100

    Linting.

--- src/Processor/SubFetch.php
+++ src/Processor/SubFetch.php
@@ -2,7 +2,6 @@
 
 namespace Merlin\Processor;
 
-
 use GuzzleHttp\Exception\BadResponseException;
 use GuzzleHttp\Exception\RequestException;
 use Merlin\Fetcher\Cache;
@@ -20,7 +19,7 @@ class SubFetch extends ProcessorOutputBase
   private function getUrl($url, $options=[]) {
     $headers = $options['headers'];
 
-  }
+  }//end getUrl()
 
 
   public function process($value)
@@ -36,23 +35,14 @@ class SubFetch extends ProcessorOutputBase
 
     $options = ($this->config['options'] ?? []);
 
-    // HMMMMMM. From cli --no-cache doesn't seem to get set right here. Mutha F.
-//    $mainConfig = $this->output->getConfig();
-
+    // HMMMMMM. TODO: From cli --no-cache doesn't seem to get set right here?
+// $mainConfig = $this->output->getConfig();
     $useCache     = ($options['cache_enabled'] ?? true);
     $cacheDir     = ($options['cache_dir'] ?? "/tmp/merlin_cache");
     $headers      = ($options['headers'] ?? []);
 
+    $useCache = TRUE;
 
-
-    $useCache = true;
-
-
-//    $sub_fetch_id = $options['sub_fetch_id'] ?? null;
-//    if (empty($sub_fetch_id)) {
-//      throw new Exception('"sub_fetch_id" required to use sub_fetch.');
-//    }
-//
     $config = $options['config'] ?? null;
     $config_file = $options['config_file'] ?? null;
     if (empty($config) && empty($config_file)) {
@@ -61,50 +51,43 @@ class SubFetch extends ProcessorOutputBase
 
     if (!empty($config)) {
       $config_data = $config;
-    }
-    else {
+    } else {
       $config_data = \Spyc::YAMLLoad($config_file);
     }
 
     // We save the fetched output in a derivative of
-    // specified config entity (for checking mainly
+    // specified config entity (for ease of checking)
     // the data is also returned to the main caller.
     $config_data['entity_type'] .= "_fetched";
 
     // Example Array config
-    //    $config_data = [
-//      'entity_type' => "review_summary",
+    // $config_data = [
+// 'entity_type' => "review_summary",
 //
-//      'mappings' => [
-//        [
-//          'field' => 'zz_title',
-//          'selector' => '//*[@id="page"]//h1[1]',
-//          'type' => 'text'
-//        ]
-//      ]
-//    ];
-
-
+// 'mappings' => [
+// [
+// 'field' => 'zz_title',
+// 'selector' => '//*[@id="page"]//h1[1]',
+// 'type' => 'text'
+// ]
+// ]
+// ];
     $config = new ArrayConfig($config_data);
-
 
     // Guzzle Uri hates Unicode chars and will ruin your day, here
     // we make sure any unicode chars are urlencoded before going in.
     $url = preg_replace_callback(
-      '/[^\x20-\x7f]/',
-      function($match) {
-        return urlencode($match[0]);
-      },
-      $url
+        '/[^\x20-\x7f]/',
+        function($match) {
+          return urlencode($match[0]);
+        },
+        $url
     );
 
     // Resolve URL.
     $uri = Psr7\Utils::uriFor($url);
     $uri = Psr7\UriResolver::resolve(Psr7\Utils::uriFor($this->crawler->getUri()), $uri);
     $url = (string) $uri;
-
-
-
 
     // Get the domain name for Guzzle.
     $u = UriString::parse($url);
@@ -141,76 +124,69 @@ class SubFetch extends ProcessorOutputBase
       }
     }
 
+    // TODO: CHECK IF PUT INTO CACHE, MIGHT NOT BE RIGHT NOW.
     // No cache hit or didn't work out, try slurp
     if (empty($data)) {
-
       $url_domainless = str_replace($base_uri, "", $url);
 
       echo "\n\nTYRING: $url_domainless \n\n";
 
       $default_headers = [
-        'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
-        'Referer' => $base_uri
+          'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+          'Referer'    => $base_uri,
       ];
 
       $headers = array_merge($default_headers, $headers);
 
-      $client = new \GuzzleHttp\Client([
-        'base_uri' => $base_uri,
-        'timeout'  => 120.0,
-        'cookies' => true,
-        'headers' => [
-          'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
-          'Referer' => "https://www.ipart.nsw.gov.au"
+    $client = new \GuzzleHttp\Client(
+        [
+            'base_uri' => $base_uri,
+            'timeout'  => 120.0,
+            'cookies'  => true,
+            'headers'  => [
+                'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+                'Referer'    => "https://www.ipart.nsw.gov.au",
+            ],
         ]
-      ]);
+    );
 
       try {
-//        $response = $client->request(
-//          'GET',
-//          $url_domainless,
-//          [
-////          'query' => $query,
+// $response = $client->request(
+// 'GET',
+// $url_domainless,
+// [
+// 'query' => $query,
 //
-//          ]
-//        );
-
+// ]
+// );
         $response = $client->get(
-          $url_domainless,
-          [
-            'curl' => [
-              CURLOPT_REFERER => $base_uri,
-              CURLOPT_SSL_VERIFYPEER => false,
-              CURLOPT_SSL_VERIFYHOST => false,
-              CURLOPT_IPRESOLVE => CURL_IPRESOLVE_V4
+            $url_domainless,
+            [
+                'curl' => [
+                    CURLOPT_REFERER        => $base_uri,
+                    CURLOPT_SSL_VERIFYPEER => false,
+                    CURLOPT_SSL_VERIFYHOST => false,
+                    CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4,
+                ],
             ]
-          ]
         );
 
         // Check headers are content html...
         $content_type = strtolower($response->getHeader('content-type')[0]);
         $ct_html = "text/html";
-        if (substr( $content_type, 0, strlen($ct_html) ) === $ct_html) {
+        if (substr($content_type, 0, strlen($ct_html)) === $ct_html) {
           // HTML GOOD TIMES
           echo "** YES GOT HTML!\n\n";
           $body = (string) $response->getBody();
-//      $json_body = $body->getContents();
+// $json_body = $body->getContents();
           $data = $fetcher->processContent($url, $body);
 
-//          file_put_contents("/tmp/zzrah.html", $body);
-
-        }
-        else {
-
+// file_put_contents("/tmp/zzrah.html", $body);
+        } else {
           // TODO: ADD TO SUBFETCH ERROR OUTPUT SAY IT'S A FILE.
           echo "ERROR: DIDNT RECEIVE TEXT/HTML!\n\n";
-
         }
-
-
-
-      }
-      catch (RequestException $e) {
+      } catch (RequestException $e) {
         var_dump($e->getMessage());
 
         $code = null;
@@ -218,50 +194,42 @@ class SubFetch extends ProcessorOutputBase
 
         if ($e->hasResponse()) {
           $response = $e->getResponse();
-          $code = $response->getStatusCode(); // HTTP status code;
-          $reason = $response->getReasonPhrase(); // Response message;
+          $code = $response->getStatusCode();
+// HTTP status code;
+          $reason = $response->getReasonPhrase();
+// Response message;
         }
 
         $d = [
-          'error' => $e->getMessage(),
-          'http_code' => $code,
-          'reason' => $reason,
-          'url' => $url,
-          'found_on' => $this->crawler->getUri()
+            'error'     => $e->getMessage(),
+            'http_code' => $code,
+            'reason'    => $reason,
+            'url'       => $url,
+            'found_on'  => $this->crawler->getUri(),
         ];
-        $this->output->addRow("subfetch-http-errors", (object)$d);
-      }
-      catch (\Exception $e) {
+        $this->output->addRow("subfetch-http-errors", (object) $d);
+      } catch (\Exception $e) {
         var_dump($e->getMessage());
         $d = [
-          'error' => $e->getMessage(),
-          'url' => $url,
-          'found_on' => $this->crawler->getUri()
+            'error'    => $e->getMessage(),
+            'url'      => $url,
+            'found_on' => $this->crawler->getUri(),
         ];
-        $this->output->addRow("subfetch-http-errors", (object)$d);
-      }
-
-
-    }
-
+        $this->output->addRow("subfetch-http-errors", (object) $d);
+      }//end try
+    }//end if
 
     $io->writeln("\n--------- END SUB FETCH ---------");
 
-
     $ret = [
-      'type' => 'fetched',
-      'url' => $url,
-      'found_on' => $this->crawler->getUri(),
-      'data' => $data,
+        'type'     => 'fetched',
+        'url'      => $url,
+        'found_on' => $this->crawler->getUri(),
+        'data'     => $data,
     ];
     return $ret;
 
+}//end process()
 
 
-
-
-  }
-
-
-
-}
+}//end class
