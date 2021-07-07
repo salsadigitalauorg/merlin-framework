bbd7120466af56181f47962fdd5084342c1c4ab9
Andrew Rowlands <andrew@firecannon.com>
2021-07-07 08:42:37 +0100

    Options for SubFetch.

--- src/Processor/SubFetch.php
+++ src/Processor/SubFetch.php
@@ -2,51 +2,74 @@
 
 namespace Merlin\Processor;
 
+use GuzzleHttp\Client;
 use GuzzleHttp\Exception\BadResponseException;
 use GuzzleHttp\Exception\RequestException;
 use Merlin\Fetcher\Cache;
 use Merlin\Fetcher\FetcherBase;
+use Merlin\Fetcher\FetcherDefaults;
 use Merlin\Parser\ArrayConfig;
 use \Exception;
 use GuzzleHttp\Psr7;
 use League\Uri\UriString;
 
 
+/**
+ * Fetches a URL, processes it according to config and returns data.
+ *
+ * You can use sub_fetch either with an existing yaml config
+ * file or specify a config inline.
+ *
+ * Sub fetch expects the input value from your field to be a link.
+ *
+ * @usage:
+ *   processor: sub_fetch
+ *   options:
+ *     config_file: 'content_config.yml'
+ *
+ * @usage:
+ *   processor: sub_fetch
+ *   options:
+ *     config:
+ *       entity_type: 'content'
+ *       mappings:
+ *        -
+ *          field: title
+ *          selector: //h1
+ *          type: text
+ *
+ */
+
 class SubFetch extends ProcessorOutputBase
 {
-
-
-  private function getUrl($url, $options=[]) {
-    $headers = $options['headers'];
-
-  }//end getUrl()
 
 
   public function process($value)
   {
 
-    // We expect a link to fetch be passed into this guy.
+    // We expect a link to fetch be passed here.
     $url = $value;
 
-    if (empty($url)) {
-      // TODO: add warning empty url
-      return;
+    $opts           = ($this->config['options'] ?? []);
+
+    // Use some of the main fetch options set.
+    $mainConfig     = $this->output->getConfig();
+    $mainFetchOpts  = ($mainConfig->getData()['fetch_options'] ?? []);
+
+    $useCache       = ($mainFetchOpts['cache_enabled'] ?? true);
+    $cacheDir       = ($mainFetchOpts['cache_dir'] ?? "/tmp/merlin_cache");
+
+    // Allow sub fetch to override global cache setting.
+    if (isset($opts['cache_enabled'])) {
+      $useCache = $opts['cache_enabled'];
     }
 
-    $options = ($this->config['options'] ?? []);
+    $headers = ($opts['headers'] ?? []);
+    $config  = ($opts['config'] ?? null);
 
-    // HMMMMMM. TODO: From cli --no-cache doesn't seem to get set right here?
-// $mainConfig = $this->output->getConfig();
-    $useCache     = ($options['cache_enabled'] ?? true);
-    $cacheDir     = ($options['cache_dir'] ?? "/tmp/merlin_cache");
-    $headers      = ($options['headers'] ?? []);
-
-    $useCache = TRUE;
-
-    $config = $options['config'] ?? null;
-    $config_file = $options['config_file'] ?? null;
+    $config_file = ($opts['config_file'] ?? null);
     if (empty($config) && empty($config_file)) {
-      throw new Exception('Either parser "config" array or "config_file" required to use sub_fetch.');
+      throw new \Exception('Either parser "config" array or "config_file" required to use sub_fetch.');
     }
 
     if (!empty($config)) {
@@ -55,30 +78,44 @@ class SubFetch extends ProcessorOutputBase
       $config_data = \Spyc::YAMLLoad($config_file);
     }
 
+    if (empty($config_data['entity_type'])) {
+      throw new \Exception('Config key "entity_type" must be specified to use sub_fetch.');
+    }
+
+    // Sub fetch options (these are the same as standard fetcher options).
+    $fetchOpts = $config_data['fetch_options'];
+    $allowRedirects = ($fetchOpts['follow_redirects'] ?? FetcherDefaults::FOLLOW_REDIRECTS);
+    $maxRedirects   = ($fetchOpts['max_redirects'] ?? FetcherDefaults::MAX_REDIRECTS);
+    $ignoreSSL      = ($fetchOpts['ignore_ssl_errors'] ?? FetcherDefaults::IGNORE_SSL_ERRORS);
+    $userAgent      = ($fetchOpts['user_agent'] ?? FetcherDefaults::USER_AGENT);
+    $referer        = ($fetchOpts['referer'] ?? null);
+    $ipResolve      = ($fetchOpts['ip_resolve'] ?? null);
+
+    $timeouts       = ($fetchOpts['timeouts'] ?? []);
+    $connectTimeout = ($timeouts['connect_timeout'] ?? FetcherDefaults::TIMEOUT_CONNECT);
+    $timeout        = ($timeouts['timeout'] ?? FetcherDefaults::TIMEOUT);
+
     // We save the fetched output in a derivative of
     // specified config entity (for ease of checking)
     // the data is also returned to the main caller.
     $config_data['entity_type'] .= "_fetched";
 
-    // Example Array config
-    // $config_data = [
-// 'entity_type' => "review_summary",
-//
-// 'mappings' => [
-// [
-// 'field' => 'zz_title',
-// 'selector' => '//*[@id="page"]//h1[1]',
-// 'type' => 'text'
-// ]
-// ]
-// ];
+    // Check for empty url now have entity_type.
+    if (empty($url)) {
+      $d = [
+          'error'    => "Empty URL provided",
+          'found_on' => $this->crawler->getUri(),
+      ];
+      $this->output->addRow("error-subfetch-{$config_data['entity_type']}", (object) $d);
+      return;
+    }
+
     $config = new ArrayConfig($config_data);
 
-    // Guzzle Uri hates Unicode chars and will ruin your day, here
-    // we make sure any unicode chars are urlencoded before going in.
+    // Make sure any unicode chars are urlencoded before passed to Guzzle Uri.
     $url = preg_replace_callback(
         '/[^\x20-\x7f]/',
-        function($match) {
+        function ($match) {
           return urlencode($match[0]);
         },
         $url
@@ -98,7 +135,17 @@ class SubFetch extends ProcessorOutputBase
 
     $io = $this->output->getIo();
 
-    $fetcher = new FetcherBase($this->output->getIo(), $this->output, $config);
+    /*
+     *  Note:
+     *  Currently subfetch is only using the Fetcher class to
+     *  process data, not do requests.  This could be changed to
+     *  use FetcherCurl and setting the success & error
+     *  callbacks to set return data to class property.  This
+     *  would remove the need for the Guzzle fetching below.
+     *
+     */
+
+    $fetcher = new FetcherBase($io, $this->output, $config);
 
     // Use cache?
     $cache = null;
@@ -107,120 +154,118 @@ class SubFetch extends ProcessorOutputBase
       $fetcher->setCache($cache);
     }
 
-    // Return data
+    // Return data.
     $data = null;
 
-    $io->writeln("\n--------- START SUB FETCH ---------");
-
+    // $io->writeln("\n--------- START SUB FETCH ---------");
     if ($cache instanceof Cache) {
       if ($cacheJson = $cache->get($url)) {
         $cacheData = json_decode($cacheJson, true);
         if (is_array($cacheData) && key_exists('contents', $cacheData) && !empty($cacheData['contents'])) {
           $contents = $cacheData['contents'];
           $redirect = ($cacheData['redirect'] ?? []);
-          $io->writeln("Fetched (cache): {$url}");
+          $io->writeln("Subfetch (cache): {$url}");
           $data = $fetcher->processContent($url, $contents, $redirect);
         }
       }
     }
 
-    // TODO: CHECK IF PUT INTO CACHE, MIGHT NOT BE RIGHT NOW.
-    // No cache hit or didn't work out, try slurp
+    // No cache hit, try request URL.
     if (empty($data)) {
       $url_domainless = str_replace($base_uri, "", $url);
 
-      echo "\n\nTYRING: $url_domainless \n\n";
+      $io->writeln("Subfetch: {$url}");
 
       $default_headers = [
-          'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
-          'Referer'    => $base_uri,
+          'User-Agent' => $userAgent,
+          'Referer'    => ($referer ?? $base_uri),
       ];
 
       $headers = array_merge($default_headers, $headers);
 
-    $client = new \GuzzleHttp\Client(
+    $client = new Client(
         [
-            'base_uri' => $base_uri,
-            'timeout'  => 120.0,
-            'cookies'  => true,
-            'headers'  => [
-                'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
-                'Referer'    => "https://www.ipart.nsw.gov.au",
-            ],
+            'base_uri'        => $base_uri,
+            'timeout'         => $timeout,
+            'connect_timeout' => $connectTimeout,
+            'cookies'         => true,
+            'headers'         => $headers,
         ]
     );
 
+      if (!empty($allowRedirects)) {
+        $redirects = [
+            'max'             => $maxRedirects,
+            'referer'         => true,
+            'track_redirects' => true,
+        ];
+      } else {
+        $redirects = false;
+      }
+
       try {
-// $response = $client->request(
-// 'GET',
-// $url_domainless,
-// [
-// 'query' => $query,
-//
-// ]
-// );
         $response = $client->get(
             $url_domainless,
             [
-                'curl' => [
+                'curl'            => [
                     CURLOPT_REFERER        => $base_uri,
-                    CURLOPT_SSL_VERIFYPEER => false,
-                    CURLOPT_SSL_VERIFYHOST => false,
-                    CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4,
+                    CURLOPT_SSL_VERIFYPEER => ($ignoreSSL ?? true),
+                    CURLOPT_SSL_VERIFYHOST => ($ignoreSSL ?? true),
+                    CURLOPT_IPRESOLVE      => FetcherBase::getCurlIpResolve($ipResolve),
                 ],
+                'allow_redirects' => $redirects,
             ]
         );
 
-        // Check headers are content html...
+        // Check headers are content html.
         $content_type = strtolower($response->getHeader('content-type')[0]);
         $ct_html = "text/html";
-        if (substr($content_type, 0, strlen($ct_html)) === $ct_html) {
-          // HTML GOOD TIMES
-          echo "** YES GOT HTML!\n\n";
+
+        if (!empty($opts['json_data'])) {
+          // Expect JSON data return, try decode and use result.
           $body = (string) $response->getBody();
-// $json_body = $body->getContents();
+          $data = json_decode($body);
+          if (json_last_error()) {
+            throw new \Exception(
+                json_last_error_msg()." when decoding JSON (json_data mode)"
+            );
+          }
+        } else if (substr($content_type, 0, strlen($ct_html)) === $ct_html) {
+          // HTML, normal parsing.
+          $body = (string) $response->getBody();
           $data = $fetcher->processContent($url, $body);
-
-// file_put_contents("/tmp/zzrah.html", $body);
         } else {
-          // TODO: ADD TO SUBFETCH ERROR OUTPUT SAY IT'S A FILE.
-          echo "ERROR: DIDNT RECEIVE TEXT/HTML!\n\n";
-        }
+          throw new \Exception("Expected text/html content-type, got: {$content_type}");
+        }//end if
       } catch (RequestException $e) {
-        var_dump($e->getMessage());
-
         $code = null;
         $reason = null;
 
         if ($e->hasResponse()) {
           $response = $e->getResponse();
           $code = $response->getStatusCode();
-// HTTP status code;
           $reason = $response->getReasonPhrase();
-// Response message;
         }
 
         $d = [
+            'url'       => $url,
             'error'     => $e->getMessage(),
             'http_code' => $code,
             'reason'    => $reason,
-            'url'       => $url,
             'found_on'  => $this->crawler->getUri(),
         ];
-        $this->output->addRow("subfetch-http-errors", (object) $d);
+        $this->output->addRow("error-subfetch", (object) $d);
       } catch (\Exception $e) {
-        var_dump($e->getMessage());
         $d = [
             'error'    => $e->getMessage(),
             'url'      => $url,
             'found_on' => $this->crawler->getUri(),
         ];
-        $this->output->addRow("subfetch-http-errors", (object) $d);
+        $this->output->addRow("error-subfetch", (object) $d);
       }//end try
     }//end if
 
-    $io->writeln("\n--------- END SUB FETCH ---------");
-
+    // $io->writeln("\n--------- END SUB FETCH ---------");
     $ret = [
         'type'     => 'fetched',
         'url'      => $url,
@@ -229,7 +274,7 @@ class SubFetch extends ProcessorOutputBase
     ];
     return $ret;
 
-}//end process()
+  }//end process()
 
 
 }//end class
